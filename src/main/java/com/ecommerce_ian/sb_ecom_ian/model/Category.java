215d2798a616e555123120ff75471efa6d71b1d8
IanPaniagua <paniagua.ian.de@gmail.com>
2025-03-01 10:10:12 +0100

    usin IDENTITY generationType strategy (@GeneratedValue)

--- src/main/java/com/ecommerce_ian/sb_ecom_ian/model/Category.java
+++ src/main/java/com/ecommerce_ian/sb_ecom_ian/model/Category.java
@@ -1,13 +1,25 @@
 package com.ecommerce_ian.sb_ecom_ian.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "categories")
 public class Category {
     private static long idCounter = 1;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
 
     public Category(Long categoryId, String categoryName) {
         this.categoryId = idCounter++;
         this.categoryName = categoryName;
+    }
+
+    public Category() {
+
     }
 
     public Long getCategoryId() {
