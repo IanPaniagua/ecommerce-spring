d53f210838ae3bfafcd9d02a6b9736b3e43a686d
IanPaniagua <paniagua.ian.de@gmail.com>
2025-03-01 15:32:32 +0100

    Using Lombok

--- src/main/java/com/ecommerce_ian/sb_ecom_ian/model/Category.java
+++ src/main/java/com/ecommerce_ian/sb_ecom_ian/model/Category.java
@@ -4,36 +4,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "categories")
+// Genera automaticamente @Getter, @Setter, @ToString, @EqualsAndHashCode y @RequiredArgsConstructor.
+@Data
+//Crea un constructor sin argumentos (public Category() {}) para JPA
+@NoArgsConstructor
+//Crea un constructor con todos los argumentos (public Category(Long categoryId, String categoryName) {})
+@AllArgsConstructor
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
-
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
-
-    public Category() {
-
-    }
-
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
 }
